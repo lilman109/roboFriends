1efb7c98a7d2665b9a15c4b162425d61325dca9a
lilman109 <akira.yoshidasjsu@gmail.com>
2020-04-26 22:52:31 +0900

    partial modification

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({id, name, email}) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow'>
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
 			<img alt='robot' src={`https://robohash.org/${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
